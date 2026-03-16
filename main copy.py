b1de161d1152327b3eee1841b10ccb0fda74b553
PratikP0317 <pate2293@purdue.edu>
2026-03-15 22:19:15 -0400

    Add calibration functionality and implement main game logic

--- main copy.py
+++ main copy.py
@@ -12,33 +12,15 @@ def main():
 
     board = manuel_board_input()
 
-    # countdown timer
-    print(3)
-    time.sleep(1)
-    print(2)
-    time.sleep(1)
-    print(1)
-    time.sleep(1)
-    print("Go!")
-    start_time = time.time()
-
     bot = Word_Hunt_Bot(board)
     bot.solve()
-
-    elapsed_time = time.time() - start_time
-    print(f"Solved after: {elapsed_time:.2f} seconds")
-
-    delay = 0.5  # Adjust this value to control the delay between tracing each word
 
     word_path_pairs = bot.get_found_words(sorted_max_len=True)
     for word, path in word_path_pairs:
         print(f"Word: {word}")
-        trace_word_path(path, config)
-        time.sleep(delay)  # Add delay between tracing each word
-        if time.time() - start_time > 70:
-            print("Stopping tracing to avoid long execution.")
-            break
     
+    
+
 
 
 if __name__ == "__main__":
